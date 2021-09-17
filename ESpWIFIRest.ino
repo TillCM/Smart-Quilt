3c9eecae2fe59b7f843aa9c24648eaecb868030e
Your Namesarinacm@gmail.com <you@example.com>
2021-09-17 18:39:15 +0200

    Full set of JSON data

--- ESpWIFIRest.ino
+++ ESpWIFIRest.ino
@@ -2,10 +2,10 @@
 #include <WiFiEspClient.h>
 #include <WiFiEspServer.h>
 #include <SoftwareSerial.h>
-#include <ArduinoJson.h>
 #include <aREST.h>
 #include <SPI.h>
 #include <WiFiEspUdp.h>
+#include<ArduinoJson.h>
 #define RX 10
 #define TX 11 
 SoftwareSerial esp8266(RX,TX);
@@ -17,10 +17,12 @@ char pass[] = "CID3208till";    // your network password (use for WPA, or use as
 int status = WL_IDLE_STATUS;
 int temperature;
 int humidity;
-int val;
+int val=0;
 int sensorVal;
 int touchPin=9;
 int reqCount = 0; 
+int correct;
+String json;
 
 
 
@@ -35,10 +37,9 @@ void setup()
   esp8266.begin(115200);
 
   // Init variables and expose them to REST API
-  temperature = 24;
-  humidity = 40;
 
-    WiFi.init(&esp8266);    // initialize ESP module
+
+   WiFi.init(&esp8266);    // initialize ESP module
    while (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
     delay(1000);
@@ -65,14 +66,16 @@ void setup()
   // start the web server on port 80
   server.begin();
   Serial.println("Server started");
+
   
 }
 
 void loop() 
 {
   sensorVal - getSensorData();
+  int correct = sensorVal++;
  WiFiEspClient client = server.available();  // listen for incoming clients
-
+  
   if (client) {                               // if you get a client,
     Serial.println("New client");             // print a message out the serial port
     buf.init();                               // initialize the circular buffer
@@ -85,7 +88,7 @@ void loop()
         // you got two newline characters in a row
         // that's the end of the HTTP request, so send a response
         if (buf.endsWith("\r\n")) {
-          delay(1000);
+          delay(8000);
           sendHttpResponse(client);
           break;
         }
@@ -107,7 +110,7 @@ void loop()
 int getSensorData(){
   val = digitalRead(touchPin); 
   if(val ==1){
-     Serial.println("Touched");
+     
   }
      return val;
 }
@@ -130,17 +133,26 @@ void printWifiStatus()
 
 void sendHttpResponse(WiFiEspClient client)
 {
+     
   client.print(
     "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/json\r\n"
+    "Content-Type: application/json\r\n"
     "Connection: close\r\n"  // the connection will be closed after completion of the response
     "Refresh: 20\r\n"        // refresh the page automatically every 20 sec
     "\r\n");
-  client.print("{\"name\": \"Hello from Quilt\"}");
+  
+ 
+
+  client.print("{\n \"LearnShapes\":\n  {\"activityID\": \"001\",\n    \"acticityName\": \"shapes\",\n    \"date\":\"12/12/2021\",\n    \"correct\": \"8\",\n     \"timeOnTask\":\"5\"\n },\n \"LearnNumbers\":\n  {\"activityID\": \"002\",\n    \"acticityName\": \"numbers\",\n    \"date\":\"12/12/2021\",\n    \"correct\": \"5\",\n     \"timeOnTask\":\"1.5\"\n },\n \"MarchShapes\":\n  {\"activityID\": \"003\",\n    \"acticityName\": \"match\",\n    \"date\":\"12/12/2021\",\n    \"correct\": \"6\",\n     \"timeOnTask\":\"3\"\n },\n \"Love\":\n  {\"activityID\": \"004\",\n    \"acticityName\": \"loves\",\n    \"date\":\"12/12/2021\",\n    \"correct\": \"8\",\n     \"timeOnTask\":\"5\"\n }\n\n}\n\n");
+  
+  Serial.print(json);
   client.print(++reqCount);
-  client.print(sensorVal);
+  client.print(correct);
   client.print("Analog input A0: ");
   client.print(analogRead(0));
  
 }
+
+
+
      
